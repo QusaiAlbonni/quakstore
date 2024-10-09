fac59da422fb777f548e58ef775e19ae8e930002
QusaiAlbonni <axinkiro@gmail.com>
2024-10-10 02:59:11 +0300

    staticfiles settings

--- QuakStore/QuakStore/settings.py
+++ QuakStore/QuakStore/settings.py
@@ -145,6 +145,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
 
