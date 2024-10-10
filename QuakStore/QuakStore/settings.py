18f79a276c00d5790214a79ee7c517e18b526df6
QusaiAlbonni <axinkiro@gmail.com>
2024-10-10 03:06:56 +0300

    staticfiles settings

--- QuakStore/QuakStore/settings.py
+++ QuakStore/QuakStore/settings.py
@@ -83,7 +83,7 @@ ROOT_URLCONF = 'QuakStore.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -145,9 +145,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR /'staticfiles'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media/'
