e9adc4d533bb59b37660a69eea02474aa90943d3
QusaiAlbonni <axinkiro@gmail.com>
2024-10-27 04:10:06 +0300

    added public user serializer

--- QuakStore/users/serializers.py
+++ QuakStore/users/serializers.py
@@ -1,4 +1,5 @@
 from djoser.serializers import UserCreateSerializer as DjoserCreateSerializer, UserSerializer as DjoserUserSerializer
+from rest_framework.fields import empty
 
 class UserCreateSerializer(DjoserCreateSerializer):
     
@@ -9,3 +10,9 @@ class UserSerializer(DjoserUserSerializer):
     
     class Meta(DjoserUserSerializer.Meta):
         fields = DjoserUserSerializer.Meta.fields + ('email', 'avatar')
+        
+class PublicUserSerializer(UserSerializer):
+    def __init__(self, instance=None, data=empty, **kwargs):
+        super().__init__(instance, data, **kwargs)
+        self.fields.pop('email')
+        self.fields.pop('id')
